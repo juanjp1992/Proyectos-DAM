dc532cc8ca7b912856feea5c207b453fa89904d4
Juan Jimenez Perez <juanjperez1992@gmail.com>
2021-10-04 16:49:20 +0200

    Tarea 1 PSP [FINALIZADA]

--- PSP/Tarea_1/tarea1PSPHijo/src/tarea1psphijo/Tarea1PSPHijo.java
+++ PSP/Tarea_1/tarea1PSPHijo/src/tarea1psphijo/Tarea1PSPHijo.java
@@ -1,41 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tarea1psphijo;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 
 /**
  *
- * @author juanj
+ * @author Juan Jimenez Perez
  */
 public class Tarea1PSPHijo {
 
     public static void main(String[] args) {
-        
+        //Creo una entrada de datos para el hijo
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try{
+            // Guardo los datos recuperados en un string, los cuales usaré para calcular las tablas.
             String numTabla = reader.readLine();
+            
+            //Ahora le digo que mientras el texto que recibamos por el flujo de datos no sea "fin" seguirá en bucle.
             while(!numTabla.equals("fin")){
                 //Recorremos con el núm que nos haya llegado las tablas multiplicando del 1 al 10.
                 for (int i = 1; i <= 10; i++) {
-                    //Imprimimos por pantalla los valores
+                    //Imprimimos los valores
                     System.out.println(numTabla + "x" + i + " = " + (Integer.parseInt(numTabla) * i));
-                    
                 }
-                //volvemos a leer el siguiente num para ver que no sea "fin".
+                //Avisamos de que la tabla ha sido finalizada, esto lo usará el padre para salir del bucle.
+                System.out.println("tabla finalizada");
+                
+                //Volvemos a leer el siguiente num para ver que no sea "fin".
                 numTabla = reader.readLine();
                
             }  
            
              
         }
-        catch(Exception ex){
-            System.out.println("Fallo procesando el dato");
+        catch(IOException | NumberFormatException ex){
+            System.out.println("ERROR: No se ha podido procesar la tabla");
             
         }
         
--- PSP/Tarea_1/tarea1PSPPadre/src/tarea1psppadre/Tarea1PSPPadre.java
+++ PSP/Tarea_1/tarea1PSPPadre/src/tarea1psppadre/Tarea1PSPPadre.java
@@ -1,19 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tarea1psppadre;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.Scanner;
 
 /**
  *
- * @author juanj
+ * @author Juan Jimenez Perez
  */
 public class Tarea1PSPPadre {
 
@@ -21,60 +16,54 @@ public class Tarea1PSPPadre {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Process hijo = null;
+        
         
         try{
-            hijo = new ProcessBuilder("java","-jar","hijo.jar").start();
-            System.out.println("Se ha creado el proceso hijo");
+            //Creo un proceso hijo a partir del archivo .jar creado a partir del otro proyecto
+            Process hijo = new ProcessBuilder("java","-jar","tarea1PSPHijo.jar").start();
             
+            //Abro un flujo de entrada hacia el hijo
             BufferedReader entradaHijo = new BufferedReader(new InputStreamReader(hijo.getInputStream()));
+            //Abro un flujo de salida del hijo, par arecibir los datos
             PrintStream salidaHijo = new PrintStream(hijo.getOutputStream());
            
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("PRUEBAAAAA");
-            String line = "inicio";
-            String cadena = "inicio";
-            int num = 1;
+            //Inicializo la variable resultado, donde almacenaré los datos recibidos del hijo
+            String resultado = "";
+            
+            //Inicio un bucle que usaré para enviar al hijo que queremos las tablas del 1 al 10.
             for (int i = 1; i <= 10; i++) {
-               salidaHijo.println(i);
-                while(!line.equals("")){
-                        salidaHijo.flush();
-                        System.out.println(line);
-                        line = entradaHijo.readLine();
-
-                } 
-                line = entradaHijo.readLine();
-            }
-            
-            
-            /*
-            while(!cadena.equals("fin")){
-
-                for (num = 1; num <= 10; num++) {
-                    salidaHijo.println(String.valueOf(num));
-                    
- 
+                //Envio al hijo el número de la tabla que vamos a querer.
+                salidaHijo.println(String.valueOf(i));
+                //Obligamos a enviar los datos.
+                salidaHijo.flush();
+                //Introducimos el titular para saber que tabla será.
+                System.out.println("TABLA DEL " + i);
                 
-          
-                while(!line.equals("")){
-                    salidaHijo.flush();
-                    System.out.println(line);
-                    line = entradaHijo.readLine();
+                //Empezamos un bucle while que recuperará las tablas, y sabrá el final de cada una usando "tabla finalizada" recibido del hijo.
+                while(!(resultado = entradaHijo.readLine()).equals("tabla finalizada")){
+                    //Imprimo las tablas
+                    System.out.println("- " + resultado);
 
                 }
-                if(num == 10){
-                    salidaHijo.print("fin");
-                    cadena = "fin";
-
-                }
-                }
-            }*/
+                //Hago un salto de linea para espaciar las tablas.
+                System.out.println("");
+            }
+            //Finalizo el programa hijo
+            salidaHijo.println("fin");
+            
+            //Imprimo lo que se pide en la tarea
+            System.out.println("******************************************\n"
+                                + "*        PSP - Tarea Individual 1        *\n"
+                                + "******************************************\n"
+                                + "*           Juan Jimenez Perez           *\n"
+                                + "******************************************\n"
+                                + "*                47619383E               *");
+                                 
         }
         catch(IOException ex){
-            
+            System.out.println("ERROR: No se pudo solicitar una tabla.");
         }
         
-        System.out.println("salio");
     }
     
 }
